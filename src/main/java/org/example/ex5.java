bb03dceed73b4328e91a0720d91edc79deb30f47
Даниил Гнитецкий <dgnitetskiy@gmail.com>
2023-09-14 00:31:30 +0300

    сделал последнее

--- src/main/java/org/example/ex5.java
+++ src/main/java/org/example/ex5.java
@@ -1,8 +1,24 @@
 package org.example;
 
+import java.math.BigInteger;
+import java.util.Scanner;
+
 public class ex5 {
-    public static void main(String[] args) {
-        fact a = new fact();
-        a.factorial();
+
+    public static void factorial(int n) {
+        BigInteger multiplication = new BigInteger("1");
+        for (int i = 1; i <= n; i++) {
+            multiplication = multiplication.multiply(BigInteger.valueOf(i));
+            System.out.println(multiplication);
+        }
+        System.out.println("result - " + multiplication);
     }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter count of numbers: ");
+        int n = in.nextInt();
+        factorial(n);
+    }
+
 }
+
